0c0ea9fbd9e3d15bdf4e56ace7294414ef9ac92d
gsudhirmadhu <57956357+gsudhirmadhu@users.noreply.github.com>
2019-11-19 23:52:36 +0530

    Update testFirst.java

--- testReposityProj/src/testReposityProj/testFirst.java
+++ testReposityProj/src/testReposityProj/testFirst.java
@@ -2,7 +2,7 @@ package testReposityProj;
 
 public class testFirst {
 
-	public static void main(String[] args) {
+	public static void main(String[] ...args) {
 		// TODO Auto-generated method stub
 
 	}
